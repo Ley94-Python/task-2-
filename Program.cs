ad34a19662195b04f36e15651ab66d33910688d3
LEY <achilova_94@list.ru>
2022-11-20 21:37:03 +0300

    added new task(2)

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/ Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите три числа");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB) 
+
+if (numberA > numberC)
+{
+Console.WriteLine($"{numberA} = Максимальное число ");
+}
+
+else {
+    Console.WriteLine($"{numberC} = Максимальное число");
+}
+
+if (numberB > numberC)
+{
+    Console.WriteLine($"{numberB} = Максимальное число ");
+}
+else {
+    Console.WriteLine($"{numberC} = Максимальное число");
+}
